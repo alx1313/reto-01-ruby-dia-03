8db392fcb3a9d5746a3c6107310cc106a940a92b
Alejandro Cabrera <alx2406@hotmail.com>
2020-06-15 17:56:25 -0500

    First code session
    On this code I started to use classes and methods to solve the game of life

---
+++ game-of-life-01.rb
@@ -0,0 +1,20 @@
+@planet = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]]
+
+class life(live)
+    @living_thing = live
+end
+
+def GetLife(x,y)
+    @new_life = @planet[x][y]
+
+
+
+end
+
+def analize_neighbors
+    
+end
